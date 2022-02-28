3126d850784e91b38c26930bd9709342050cd582
Raphael Scursoni <ra.scursoni@gmail.com>
2022-02-28 18:57:42 -0300

    Modificando a estrutura das Properties

--- Properties/Produto.cs
+++ Properties/Produto.cs
@@ -4,16 +4,16 @@ namespace Course
     class Produto
     {
         private string _nome;
-        private double _preco;
-        private int _quantidade;
+        public double Preco { get; private set; }
+        public int Quantidade  { get; private set; }
         public Produto()
         {
         }
         public Produto(string nome, double preco, int quantidade)
         {
             _nome = nome;
-            _preco = preco;
-            _quantidade = quantidade;
+            Preco = preco;
+            Quantidade = quantidade;
         }
         public string Nome
         {
@@ -26,33 +26,25 @@ namespace Course
                 }
             }
         }
-        public double Preco
-        {
-            get { return _preco; }
-        }
-        public int Quantidade
-        {
-            get { return _quantidade; }
-        }
         public double ValorTotalEmEstoque
         {
-            get { return _preco * _quantidade; }
+            get { return Preco * Quantidade; }
         }
         public void AdicionarProdutos(int quantidade)
         {
-            _quantidade += quantidade;
+            Quantidade += quantidade;
         }
         public void RemoverProdutos(int quantidade)
         {
-            _quantidade -= quantidade;
+            Quantidade -= quantidade;
         }
         public override string ToString()
         {
             return _nome
             + ", $ "
-            + _preco.ToString("F2", CultureInfo.InvariantCulture)
+            + Preco.ToString("F2", CultureInfo.InvariantCulture)
             + ", "
-            + _quantidade
+            + Quantidade
             + " unidades, Total: $ "
             + ValorTotalEmEstoque.ToString("F2", CultureInfo.InvariantCulture);
         }
